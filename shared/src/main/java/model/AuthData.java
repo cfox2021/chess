227e48fd39b71d67c1ec46d0ebf3e7b0710f6ab9
Chace Fox <cfox2021@byu.edu>
2025-02-21 19:14:49 -0700

    Implemented GameData, AuthData, and UserData including getter functions.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,19 @@
 package model;
 
 public class AuthData {
+    private String username;
+    private String authToken;
+
+    AuthData(String username, String authToken) {
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
 }
