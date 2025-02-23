a3dadcf8c9290263bd55d633c09851cd30ab6404
Chace Fox <cfox2021@byu.edu>
2025-02-22 17:49:47 -0700

    Updated GameData, AuthData, and UserData to be record classes.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,19 +1,4 @@
 package model;
 
-public class AuthData {
-    private String username;
-    private String authToken;
-
-    AuthData(String username, String authToken) {
-        this.username = username;
-        this.authToken = authToken;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getAuthToken() {
-        return authToken;
-    }
+public record AuthData(String username, String authToken) {
 }
