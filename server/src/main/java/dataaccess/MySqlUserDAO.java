67e02559e97749b8e6f010d9ff1862b7e8df1823
Chace Fox <cfox2021@byu.edu>
2025-03-08 19:56:22 -0700

    Initial implementation of CreateStatement and configureDatabase function for UserDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,8 +1,42 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.UserData;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySqlUserDAO implements UserDAO {
+
+    public MySqlUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could Not Configure Database: from USER DAO");
+        }
+    }
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  userData (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
     @Override
     public UserData getUserData(String username) throws DataAccessException {
         return null;
